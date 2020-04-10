88fc3f16faf6cbd8d565feb34cec0278414edf4a
Moustanir <moustanir.soule-ali@ynov.com>
2020-04-10 10:43:23 +0200

    Maths quizz program

---
+++ Quizz/Form1.Designer.cs
@@ -0,0 +1,321 @@
+﻿namespace Quizz
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.startQuizz = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.elementOperation1 = new System.Windows.Forms.Label();
+            this.plusSIgn = new System.Windows.Forms.Label();
+            this.elementOperation2 = new System.Windows.Forms.Label();
+            this.equalSign1 = new System.Windows.Forms.Label();
+            this.elementOperation3 = new System.Windows.Forms.Label();
+            this.multiplySign = new System.Windows.Forms.Label();
+            this.elementOperation4 = new System.Windows.Forms.Label();
+            this.equalSign2 = new System.Windows.Forms.Label();
+            this.elementOperation5 = new System.Windows.Forms.Label();
+            this.minusSign = new System.Windows.Forms.Label();
+            this.elementOperation6 = new System.Windows.Forms.Label();
+            this.equalSign3 = new System.Windows.Forms.Label();
+            this.endQuizz = new System.Windows.Forms.Button();
+            this.response1 = new System.Windows.Forms.TextBox();
+            this.response2 = new System.Windows.Forms.TextBox();
+            this.response3 = new System.Windows.Forms.TextBox();
+            this.hiddenResponse2 = new System.Windows.Forms.Label();
+            this.hiddenResponse3 = new System.Windows.Forms.Label();
+            this.hiddenResponse1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // startQuizz
+            // 
+            this.startQuizz.Location = new System.Drawing.Point(418, 21);
+            this.startQuizz.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.startQuizz.Name = "startQuizz";
+            this.startQuizz.Size = new System.Drawing.Size(90, 33);
+            this.startQuizz.TabIndex = 0;
+            this.startQuizz.Text = "Start Quizz";
+            this.startQuizz.UseVisualStyleBackColor = true;
+            this.startQuizz.Click += new System.EventHandler(this.startQuizz_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(255, 31);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(56, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "TimeLabel";
+            // 
+            // elementOperation1
+            // 
+            this.elementOperation1.AutoSize = true;
+            this.elementOperation1.Location = new System.Drawing.Point(41, 114);
+            this.elementOperation1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.elementOperation1.Name = "elementOperation1";
+            this.elementOperation1.Size = new System.Drawing.Size(0, 13);
+            this.elementOperation1.TabIndex = 2;
+            // 
+            // plusSIgn
+            // 
+            this.plusSIgn.AutoSize = true;
+            this.plusSIgn.Location = new System.Drawing.Point(148, 114);
+            this.plusSIgn.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.plusSIgn.Name = "plusSIgn";
+            this.plusSIgn.Size = new System.Drawing.Size(13, 13);
+            this.plusSIgn.TabIndex = 3;
+            this.plusSIgn.Text = "+";
+            // 
+            // elementOperation2
+            // 
+            this.elementOperation2.AutoSize = true;
+            this.elementOperation2.Location = new System.Drawing.Point(218, 114);
+            this.elementOperation2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.elementOperation2.Name = "elementOperation2";
+            this.elementOperation2.Size = new System.Drawing.Size(0, 13);
+            this.elementOperation2.TabIndex = 4;
+            // 
+            // equalSign1
+            // 
+            this.equalSign1.AutoSize = true;
+            this.equalSign1.Location = new System.Drawing.Point(303, 114);
+            this.equalSign1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.equalSign1.Name = "equalSign1";
+            this.equalSign1.Size = new System.Drawing.Size(13, 13);
+            this.equalSign1.TabIndex = 5;
+            this.equalSign1.Text = "=";
+            // 
+            // elementOperation3
+            // 
+            this.elementOperation3.AutoSize = true;
+            this.elementOperation3.Location = new System.Drawing.Point(40, 161);
+            this.elementOperation3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.elementOperation3.Name = "elementOperation3";
+            this.elementOperation3.Size = new System.Drawing.Size(0, 13);
+            this.elementOperation3.TabIndex = 6;
+            // 
+            // multiplySign
+            // 
+            this.multiplySign.AutoSize = true;
+            this.multiplySign.Location = new System.Drawing.Point(148, 160);
+            this.multiplySign.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.multiplySign.Name = "multiplySign";
+            this.multiplySign.Size = new System.Drawing.Size(11, 13);
+            this.multiplySign.TabIndex = 7;
+            this.multiplySign.Text = "*";
+            // 
+            // elementOperation4
+            // 
+            this.elementOperation4.AutoSize = true;
+            this.elementOperation4.Location = new System.Drawing.Point(218, 160);
+            this.elementOperation4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.elementOperation4.Name = "elementOperation4";
+            this.elementOperation4.Size = new System.Drawing.Size(0, 13);
+            this.elementOperation4.TabIndex = 8;
+            // 
+            // equalSign2
+            // 
+            this.equalSign2.AutoSize = true;
+            this.equalSign2.Location = new System.Drawing.Point(303, 160);
+            this.equalSign2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.equalSign2.Name = "equalSign2";
+            this.equalSign2.Size = new System.Drawing.Size(13, 13);
+            this.equalSign2.TabIndex = 9;
+            this.equalSign2.Text = "=";
+            // 
+            // elementOperation5
+            // 
+            this.elementOperation5.AutoSize = true;
+            this.elementOperation5.Location = new System.Drawing.Point(41, 217);
+            this.elementOperation5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.elementOperation5.Name = "elementOperation5";
+            this.elementOperation5.Size = new System.Drawing.Size(0, 13);
+            this.elementOperation5.TabIndex = 10;
+            // 
+            // minusSign
+            // 
+            this.minusSign.AutoSize = true;
+            this.minusSign.Location = new System.Drawing.Point(150, 216);
+            this.minusSign.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.minusSign.Name = "minusSign";
+            this.minusSign.Size = new System.Drawing.Size(10, 13);
+            this.minusSign.TabIndex = 11;
+            this.minusSign.Text = "-";
+            // 
+            // elementOperation6
+            // 
+            this.elementOperation6.AutoSize = true;
+            this.elementOperation6.Location = new System.Drawing.Point(218, 216);
+            this.elementOperation6.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.elementOperation6.Name = "elementOperation6";
+            this.elementOperation6.Size = new System.Drawing.Size(0, 13);
+            this.elementOperation6.TabIndex = 12;
+            // 
+            // equalSign3
+            // 
+            this.equalSign3.AutoSize = true;
+            this.equalSign3.Location = new System.Drawing.Point(303, 217);
+            this.equalSign3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.equalSign3.Name = "equalSign3";
+            this.equalSign3.Size = new System.Drawing.Size(13, 13);
+            this.equalSign3.TabIndex = 13;
+            this.equalSign3.Text = "=";
+            // 
+            // endQuizz
+            // 
+            this.endQuizz.Location = new System.Drawing.Point(257, 301);
+            this.endQuizz.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.endQuizz.Name = "endQuizz";
+            this.endQuizz.Size = new System.Drawing.Size(56, 19);
+            this.endQuizz.TabIndex = 14;
+            this.endQuizz.Text = "End Quizz";
+            this.endQuizz.UseVisualStyleBackColor = true;
+            this.endQuizz.Visible = false;
+            this.endQuizz.Click += new System.EventHandler(this.endQuizz_Click);
+            // 
+            // response1
+            // 
+            this.response1.Enabled = false;
+            this.response1.Location = new System.Drawing.Point(360, 114);
+            this.response1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.response1.Name = "response1";
+            this.response1.Size = new System.Drawing.Size(76, 20);
+            this.response1.TabIndex = 15;
+            // 
+            // response2
+            // 
+            this.response2.Enabled = false;
+            this.response2.Location = new System.Drawing.Point(360, 156);
+            this.response2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.response2.Name = "response2";
+            this.response2.Size = new System.Drawing.Size(76, 20);
+            this.response2.TabIndex = 16;
+            // 
+            // response3
+            // 
+            this.response3.Enabled = false;
+            this.response3.Location = new System.Drawing.Point(360, 212);
+            this.response3.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.response3.Name = "response3";
+            this.response3.Size = new System.Drawing.Size(76, 20);
+            this.response3.TabIndex = 17;
+            // 
+            // hiddenResponse2
+            // 
+            this.hiddenResponse2.AutoSize = true;
+            this.hiddenResponse2.Enabled = false;
+            this.hiddenResponse2.Location = new System.Drawing.Point(471, 160);
+            this.hiddenResponse2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.hiddenResponse2.Name = "hiddenResponse2";
+            this.hiddenResponse2.Size = new System.Drawing.Size(35, 13);
+            this.hiddenResponse2.TabIndex = 19;
+            this.hiddenResponse2.Text = "label5";
+            // 
+            // hiddenResponse3
+            // 
+            this.hiddenResponse3.AutoSize = true;
+            this.hiddenResponse3.Enabled = false;
+            this.hiddenResponse3.Location = new System.Drawing.Point(473, 216);
+            this.hiddenResponse3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.hiddenResponse3.Name = "hiddenResponse3";
+            this.hiddenResponse3.Size = new System.Drawing.Size(35, 13);
+            this.hiddenResponse3.TabIndex = 20;
+            this.hiddenResponse3.Text = "label7";
+            // 
+            // hiddenResponse1
+            // 
+            this.hiddenResponse1.AutoSize = true;
+            this.hiddenResponse1.Enabled = false;
+            this.hiddenResponse1.Location = new System.Drawing.Point(468, 117);
+            this.hiddenResponse1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.hiddenResponse1.Name = "hiddenResponse1";
+            this.hiddenResponse1.Size = new System.Drawing.Size(35, 13);
+            this.hiddenResponse1.TabIndex = 18;
+            this.hiddenResponse1.Text = "label3";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(600, 366);
+            this.Controls.Add(this.hiddenResponse3);
+            this.Controls.Add(this.hiddenResponse2);
+            this.Controls.Add(this.hiddenResponse1);
+            this.Controls.Add(this.response3);
+            this.Controls.Add(this.response2);
+            this.Controls.Add(this.response1);
+            this.Controls.Add(this.endQuizz);
+            this.Controls.Add(this.equalSign3);
+            this.Controls.Add(this.elementOperation6);
+            this.Controls.Add(this.minusSign);
+            this.Controls.Add(this.elementOperation5);
+            this.Controls.Add(this.equalSign2);
+            this.Controls.Add(this.elementOperation4);
+            this.Controls.Add(this.multiplySign);
+            this.Controls.Add(this.elementOperation3);
+            this.Controls.Add(this.equalSign1);
+            this.Controls.Add(this.elementOperation2);
+            this.Controls.Add(this.plusSIgn);
+            this.Controls.Add(this.elementOperation1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.startQuizz);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "Form1";
+            this.Text = "QUIZ - Let\'s check your maths";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button startQuizz;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label elementOperation1;
+        private System.Windows.Forms.Label plusSIgn;
+        private System.Windows.Forms.Label elementOperation2;
+        private System.Windows.Forms.Label equalSign1;
+        private System.Windows.Forms.Label elementOperation3;
+        private System.Windows.Forms.Label multiplySign;
+        private System.Windows.Forms.Label elementOperation4;
+        private System.Windows.Forms.Label equalSign2;
+        private System.Windows.Forms.Label elementOperation5;
+        private System.Windows.Forms.Label minusSign;
+        private System.Windows.Forms.Label elementOperation6;
+        private System.Windows.Forms.Label equalSign3;
+        private System.Windows.Forms.Button endQuizz;
+        private System.Windows.Forms.TextBox response1;
+        private System.Windows.Forms.TextBox response2;
+        private System.Windows.Forms.TextBox response3;
+        private System.Windows.Forms.Label hiddenResponse2;
+        private System.Windows.Forms.Label hiddenResponse3;
+        private System.Windows.Forms.Label hiddenResponse1;
+    }
+}
+
---
+++ Quizz/Form1.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Quizz
+{
+    public partial class Form1 : Form
+    {
+        Random randomizer = new Random();
+        int result1, result2, result3;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void startQuizz_Click(object sender, EventArgs e)
+        {
+            //
+            elementOperation1.Text = randomizer.Next(10).ToString();
+            elementOperation2.Text = randomizer.Next(10).ToString();
+            elementOperation3.Text = randomizer.Next(10).ToString();
+            elementOperation4.Text = randomizer.Next(10).ToString();
+            elementOperation5.Text = randomizer.Next(10).ToString();
+            elementOperation6.Text = randomizer.Next(10).ToString();
+            result1 = Int32.Parse(elementOperation1.Text) + Int32.Parse(elementOperation2.Text);
+            result2 = Int32.Parse(elementOperation3.Text) * Int32.Parse(elementOperation4.Text);
+            result3 = Int32.Parse(elementOperation5.Text) - Int32.Parse(elementOperation6.Text);
+
+            startQuizz.Enabled = false;
+            startQuizz.Visible = false;
+            endQuizz.Enabled = true;
+            startQuizz.Visible = false;
+            endQuizz.Visible = true;
+            response1.Enabled = true;
+            response2.Enabled = true;
+            response3.Enabled = true;
+        }
+
+        // When user wish to end the quizz
+        private void endQuizz_Click(object sender, EventArgs e)
+        {
+            // Collect response
+            // Compare
+            // Give the number of good response
+            int response1Int, response2Int, response3Int;
+            var isParsed = Int32.TryParse(response1.Text, out response1Int);
+            var isParsed2 = Int32.TryParse(response2.Text, out response2Int);
+            var isParsed3 = Int32.TryParse(response3.Text, out response3Int);
+            if(response1Int == result1)
+            {
+                hiddenResponse1.Text = "Good";
+            }
+            else
+            {
+                hiddenResponse1.Text = $"Fail. Response {result1}";
+            }
+            if (response1Int == result1)
+            {
+                hiddenResponse1.Text = "Good";
+            }
+            else
+            {
+                hiddenResponse1.Text = "Fail";
+            }
+            if (response2Int == result2)
+            {
+                hiddenResponse2.Text = "Good";
+            }
+            else
+            {
+                hiddenResponse2.Text = $"Fail. Response {result1}";
+            }
+            if (response3Int == result3)
+            {
+                hiddenResponse3.Text = "Good";
+            }
+            else
+            {
+                hiddenResponse3.Text = $"Fail. Response {result3}";
+            }
+            hiddenResponse1.Visible = true;
+            hiddenResponse2.Visible = true;
+            hiddenResponse3.Visible = true;
+            endQuizz.Visible = false;
+            response1.Enabled = false;
+            response2.Enabled = false;
+            response3.Enabled = false;
+        }
+    }
+}
