bcd51d860dee75b547fa65658793c9bd6bf5b875
Moustanir <moustanir.soule-ali@ynov.com>
2020-04-12 21:06:21 +0200

    Join thread

--- Csharp-Threads/Program.cs
+++ Csharp-Threads/Program.cs
@@ -1,27 +1,30 @@
 ﻿using System;
 using System.Threading;
+using System.Threading.Tasks;
 
 namespace Csharp_Threads
 {
     class Program
     {
-        static void NewThread()
+        static void CustomThread()
         {
-            Console.WriteLine("I am a new thread");
-            Thread.Sleep(1000);
-            Console.WriteLine("Thread finished to sleep.");
+            for (int index = 0; index < 100; index++)
+            {
+                Console.WriteLine("Thread Process: {0}", index);
+                Thread.Sleep(0);
+            }
         }
 
         static void Main(string[] args)
         {
-            Thread th = Thread.CurrentThread;
-            th.Name = "This is the Main Thread";
-            Console.WriteLine($"Starting new thread...");
-            ThreadStart newTh = new ThreadStart(NewThread);
-            Thread thread = new Thread(newTh);
-            thread.Start();
-            Console.WriteLine($"Ending new thread...");
-            Console.ReadLine();
+            Thread t = new Thread(new ThreadStart(CustomThread));
+            t.Start();
+            for (int index = 0; index < 10; index++)
+            {
+                Console.WriteLine("This is the main the main thread !");
+                Thread.Sleep(0);
+            }
+            t.Join();
         }
     }
 }
