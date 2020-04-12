8371fee9e9b9ad77ef95d9a1862dd62eddf1e1a3
Moustanir <moustanir.soule-ali@ynov.com>
2020-04-12 20:37:37 +0200

    Create Thread

---
+++ Csharp-Threads/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Threading;
+
+namespace Csharp_Threads
+{
+    class Program
+    {
+        static void NewThread()
+        {
+            Console.WriteLine("I am a new thread");
+        }
+
+        static void Main(string[] args)
+        {
+            Thread th = Thread.CurrentThread;
+            th.Name = "This is the Main Thread";
+            Console.WriteLine($"Starting new thread...");
+            ThreadStart newTh = new ThreadStart(NewThread);
+            Thread thread = new Thread(newTh);
+            Console.WriteLine($"Ending new thread...");
+            Console.ReadLine();
+        }
+    }
+}
